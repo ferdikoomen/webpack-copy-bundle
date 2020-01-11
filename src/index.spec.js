4c35dc78cccc28760844a65416c3f262dc26e0a1
Ferdi Koomen <info@madebyferdi.com>
2020-01-12 00:02:56 +0100

    - Added code coverage

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,20 +2,34 @@
 
 const fs = require('fs');
 const path = require('path');
+const webpack = require('webpack');
+const options = require('../webpack.config.js');
 
-test('is copied', () => {
-    const sourcePath = path.resolve(process.cwd(), './test/dist/bundle.js');
-    const targetPath = path.resolve(process.cwd(), './test/tmp/bundle.js');
+test('is copied', done => {
 
-    const sourceExists = fs.existsSync(sourcePath);
-    const targetExists = fs.existsSync(targetPath);
+    webpack(options, (err, stats) => {
 
-    const sourceStats = fs.statSync(sourcePath);
-    const targetStats = fs.statSync(targetPath);
+        if (err) {
+            return done(err);
+        } else if (stats.hasErrors()) {
+            return done(stats.toString());
+        }
 
-    expect(sourceExists).toBeTruthy();
-    expect(targetExists).toBeTruthy();
-    expect(sourceStats.size).toBeGreaterThan(0);
-    expect(targetStats.size).toBeGreaterThan(0);
-    expect(sourceStats.size).toEqual(targetStats.size);
+        const sourcePath = path.resolve(process.cwd(), './test/dist/bundle.js');
+        const targetPath = path.resolve(process.cwd(), './test/tmp/bundle.js');
+
+        const sourceExists = fs.existsSync(sourcePath);
+        const targetExists = fs.existsSync(targetPath);
+
+        const sourceStats = fs.statSync(sourcePath);
+        const targetStats = fs.statSync(targetPath);
+
+        expect(sourceExists).toBeTruthy();
+        expect(targetExists).toBeTruthy();
+        expect(sourceStats.size).toBeGreaterThan(0);
+        expect(targetStats.size).toBeGreaterThan(0);
+        expect(sourceStats.size).toEqual(targetStats.size);
+
+        done();
+    })
 });
